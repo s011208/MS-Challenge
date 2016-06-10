999e2b8dca5bc9b3123ac1ddc78342b220e549ac
s011208 <s011208@gmail.com>
2016-06-10 15:40:34 +0800

    add back navigation button in Preference activity

--- app/src/main/java/bj4/yhh/mschallenge/settings/MsChallengePreference.java
+++ app/src/main/java/bj4/yhh/mschallenge/settings/MsChallengePreference.java
@@ -1,16 +1,18 @@
 package bj4.yhh.mschallenge.settings;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import bj4.yhh.mschallenge.R;
 
 /**
  * Created by User on 2016/6/10.
  */
-public class MsChallengePreference extends Activity {
+public class MsChallengePreference extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,21 @@ public class MsChallengePreference extends Activity {
         AboutPreferenceFragment prefFragment = new AboutPreferenceFragment();
         transaction.add(R.id.fragment, prefFragment);
         transaction.commit();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
